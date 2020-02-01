d530b81dfb4693f2dccac84a82d9df8b85f62ce7
Zdravko Velinov <z.velinov@3dgraphics.guru>
2020-02-01 13:15:46 -0800

    added firing at rotating platform mechanic

--- Assets/Scripts/RockRotator.cs
+++ Assets/Scripts/RockRotator.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class RockRotator : MonoBehaviour
 {
+    public float RotationSpeed = 10;
+    public float HoldInterval = 1;
+
+    float CurrentAngle = 0;
+    float LastHold = 0;
 
     void Start()
     {
-        
+        LastHold = Time.time;
     }
 
+    public void HoldRocks()
+    {
+        LastHold = Time.time;
+    }
 
     void Update()
     {
-        
+        if(Time.time - LastHold > HoldInterval)
+            CurrentAngle += RotationSpeed * Time.deltaTime;
+        transform.rotation = Quaternion.Euler(0, 0, CurrentAngle);
+    }
+
+    void OnShoot()
+    {
+        Debug.Log("Shoot");
     }
 }
