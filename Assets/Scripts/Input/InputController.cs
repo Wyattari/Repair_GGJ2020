1145425a71b85cdac27506fb831c78d27f671a4d
Neil Sarkar <neil.r.sarkar@gmail.com>
2020-02-01 13:10:57 -0800

    Add rewired and hook up 4 player controllers

---
+++ Assets/Scripts/Input/InputController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Rewired;
+
+public class InputController : MonoBehaviour {
+	List<Player> players = new List<Player>();
+
+	// Start is called before the first frame update
+	void Start() {
+		for (int i = 0; i < 4; i++) {
+			players.Add(ReInput.players.GetPlayer(i));
+		}
+	}
+
+	// Update is called once per frame
+	void Update() {
+		foreach (var player in players) {
+			if (player.GetButtonDown("Fire")) {
+				Debug.Log($"id={player.id} MoveHorizontal={player.GetAxis("MoveHorizontal")} MoveVertical={player.GetAxis("MoveVertical")} AimHorizontal={player.GetAxis("AimHorizontal")} AimVertical={player.GetAxis("AimVertical")} Fire={player.GetButton("Fire")} Jump={player.GetButton("Jump")}");
+			}
+		}
+	}
+}
