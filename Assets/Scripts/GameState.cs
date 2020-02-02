957186761bce04fb8d5ffcb9f229735f72f3e5d0
Neil Sarkar <neil.r.sarkar@gmail.com>
2020-02-01 20:59:59 -0800

    Add players to GameState in GameManager

---
+++ Assets/Scripts/GameState.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace State {
+	[Serializable]
+	public class GameState {
+		public List<Player> Players = new List<Player>();
+	}
+
+	[Serializable]
+	public class Player {
+		public int Id;
+		public bool IsAlive;
+		public GameObject GameObject;
+	}
+}
+
