8e3cce5315bf6d8799a088a9f11e5d162d1769bd
Neil Sarkar <neil.r.sarkar@gmail.com>
2020-02-02 09:56:53 -0800

    Remove editor log from shit

--- Assets/Scripts/AudioController.cs
+++ Assets/Scripts/AudioController.cs
@@ -14,8 +14,12 @@ public class AudioController : MonoBehaviour {
 	FMOD.Studio.EventInstance jump;
 	FMOD.Studio.EventInstance land;
 
+	public static AudioController Instance;
+
 	// Start is called before the first frame update
 	void Start() {
+		if (Instance != null) { throw new System.Exception("Tried to create two AudioControllers"); }
+		Instance = this;
 		Subscribe();
 		fire = FMODUnity.RuntimeManager.CreateInstance(Fire);
 		hit = FMODUnity.RuntimeManager.CreateInstance(Hit);
@@ -26,13 +30,19 @@ public class AudioController : MonoBehaviour {
 	void Subscribe() {
 		Unsubscribe();
 		GameManager.Instance.Events.OnPlayerJump += Events_OnPlayerJump;
+		GameManager.Instance.Events.OnPlayerFire += Events_OnPlayerFire;
 	}
 
 	void Unsubscribe() {
 		GameManager.Instance.Events.OnPlayerJump -= Events_OnPlayerJump;
+		GameManager.Instance.Events.OnPlayerFire -= Events_OnPlayerFire;
 	}
 
 	void Events_OnPlayerJump(int playerId) {
 		jump.start();
+	}
+
+	void Events_OnPlayerFire(int playerId) {
+		fire.start();
 	}
 }
