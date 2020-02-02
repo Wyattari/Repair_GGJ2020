986555463efab8588821a0b6ce406f4ebfe83bd7
Wyatt <wellwyatt@gmail.com>
2020-02-01 20:51:06 -0800

    making out of bounds destroy player

--- Assets/Scripts/OutOfBoundsReset.cs
+++ Assets/Scripts/OutOfBoundsReset.cs
@@ -6,12 +6,9 @@ public class OutOfBoundsReset : MonoBehaviour
 {
     [SerializeField] LayerMask outOfBoundsLayer;
 
-    private void OnTriggerEnter(Collider other) {
-        
-    }
     private void OnCollisionEnter(Collision collision) {
         if ((outOfBoundsLayer & 1 << collision.gameObject.layer) == 1 << collision.gameObject.layer) {
-            StartCoroutine(GameManager.Instance.Reset());
+            Destroy(gameObject);
         }
     }
  
