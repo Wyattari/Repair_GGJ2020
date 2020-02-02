c1493ecacf3fe1706b1425a81a2af4f125c1af16
Matthew Wagar <mrw77@drexel.edu>
2020-02-02 15:16:27 -0800

    Shadows plus checkpoint beams

---
+++ Assets/Scripts/RaycastShadow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaycastShadow : MonoBehaviour
+{
+    public GameObject SpriteShadow;
+
+    void Update()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, -Vector3.up, out hit))
+        {
+            SpriteShadow.transform.position = hit.point;
+            SpriteShadow.transform.rotation = Quaternion.FromToRotation(transform.forward, hit.normal);
+
+            // SpriteShado
+        }
+    }
+}
